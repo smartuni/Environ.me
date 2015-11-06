68f622e0c895fa7a1d2ee698a9561208c4baad53
Ruediger Bartz <ruediger.bartz@haw-hamburg.de>
2015-11-06 11:51:48 +0100

    Moved client receive into a seperated thread to prevent kernel panic.

--- coap_client/main.c
+++ coap_client/main.c
@@ -6,6 +6,7 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include "thread.h"
 #include "shell.h"
 #include "coap.h"
 
@@ -14,6 +15,7 @@
 #define PORT              (5683)
 #define MAX_RESPONSE_SIZE (64)
 
+static void *client_recv(void *arg);
 static int request_temperature(int argc, char **argv);
 static void dumpHeader(coap_header_t *hdr);
 static void dump(const uint8_t *buf, size_t buflen, bool bare);
@@ -27,7 +29,8 @@ static const shell_command_t shell_commands[] =
 };
 static const coap_endpoint_path_t temperature_path = {1, {"temperature"}};
 
-static int client_socket = -1;
+static char thread_stack[THREAD_STACKSIZE_DEFAULT];
+static msg_t msg_queue[MSG_QUEUE_SIZE];
 static uint8_t client_buffer[BUFFER_SIZE];
 static uint16_t message_id = 1;
 
@@ -44,6 +47,10 @@ int main(void) {
     printf("[main] This board features a(n) %s MCU.\n", RIOT_MCU);
     puts("[main] ================");
 
+    thread_create(thread_stack, sizeof(thread_stack), THREAD_PRIORITY_MAIN,
+		  CREATE_STACKTEST, client_recv, NULL, "client_receiver");
+    
+
     // start shell
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
@@ -51,16 +58,72 @@ int main(void) {
     return 0;
 }
 
-static int request_temperature(int argc, char **argv) {
-    struct sockaddr_in6 server_addr;
-    size_t server_addr_len = sizeof(server_addr);
+static void *client_recv(void *arg) {
+    int client_socket = -1;
     struct sockaddr_in6 client_addr;
     size_t client_addr_len = sizeof(client_addr);
+    uint16_t port;
+    (void) arg;
+    msg_init_queue(msg_queue, MSG_QUEUE_SIZE);
+    port = (uint16_t)PORT;
+    if (port == 0) {
+        puts("[client_recv] ERROR: invalid port specified");
+        return NULL;
+    }
+    client_socket = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    if (client_socket < 0) {
+        puts("[client_recv] ERROR: initializing socket");
+        client_socket = 0;
+        return NULL;
+    }
+    client_addr.sin6_family = AF_INET6;
+    memset(&client_addr.sin6_addr, 0, sizeof(client_addr.sin6_addr));
+    client_addr.sin6_port = htons(port);
+    puts("bind");
+    if (bind(client_socket, (struct sockaddr *)&client_addr,
+	     client_addr_len) < 0) {
+        client_socket = -1;
+        puts("[client_recv] ERROR: binding socket");
+        return NULL;
+    }
+    printf("[client_recv] INFO: started client receiver on port %" PRIu16 "\n",
+	   port);
+    while (1) {
+	struct sockaddr_in6 server_addr;
+	size_t server_addr_len = sizeof(server_addr);
+	size_t buffer_size = sizeof(client_buffer);
+	int rc;
+	int recv_len;
+	// blocking receive, waiting for data
+	if ((recv_len = recvfrom(client_socket, client_buffer, buffer_size, 0,
+				 (struct sockaddr *)&server_addr,
+				 &server_addr_len)) < 0) {
+	    puts("[client_recv] ERROR: on receive");
+	} else if (recv_len == 0) {
+	    puts("[client_recv] INFO:  peer did shut down");
+	} else {
+	    // CoAP part
+	    coap_packet_t inpkt;
+	    if (0 != (rc = coap_parse(&inpkt, client_buffer, recv_len))) {
+		printf("[client_recv] INFO:  bad packet rc=%d\n", rc);
+	    } else {
+		puts("[client_recv] INFO:  redeived packet");
+	        dump(client_buffer, recv_len, true);
+		puts("\ncontent:");
+		dumpPacket(&inpkt);
+	    }
+	}
+    }
+    return NULL;
+}
+
+static int request_temperature(int argc, char **argv) {
+    int client_socket = -1;
+    struct sockaddr_in6 server_addr;
+    size_t server_addr_len = sizeof(server_addr);
     size_t buffer_size = sizeof(client_buffer);
     uint16_t port;
     int rc;
-    int recv_len;
-    coap_packet_t inpkt;
     if (argc != 2) {
 	puts("[main] INFO:  usage: temperature <addr>");
 	return 1;
@@ -82,15 +145,6 @@ static int request_temperature(int argc, char **argv) {
 	return 1;
     }
     server_addr.sin6_port = htons(port);
-    client_addr.sin6_family = AF_INET6;
-    memset(&client_addr.sin6_addr, 0, sizeof(client_addr.sin6_addr));
-    client_addr.sin6_port = htons(port);
-    if (bind(client_socket, (struct sockaddr *)&client_addr,
-	     client_addr_len) < 0) {
-        client_socket = -1;
-        puts("[main] ERROR: binding socket");
-        return 1;
-    }
     puts("[main] INFO:  creating CoAP GET request");
     coap_header_t request_hdr = {
 	.ver = 1,
@@ -134,24 +188,6 @@ static int request_temperature(int argc, char **argv) {
     if (sendto(client_socket, client_buffer, buffer_size, 0,
 	       (struct sockaddr *)&server_addr, server_addr_len) < 0) {
 	puts("[main] ERROR: sending data");
-    }
-    // blocking receive, waiting for data
-    if ((recv_len = recvfrom(client_socket, client_buffer, buffer_size, 0,
-			     (struct sockaddr *)&server_addr,
-			     &server_addr_len)) < 0) {
-	puts("[main] ERROR: on receive");
-    } else if (recv_len == 0) {
-	puts("[main] INFO:  peer did shut down");
-    } else {
-	// CoAP part
-	if (0 != (rc = coap_parse(&inpkt, client_buffer, recv_len))) {
-	    printf("[main] INFO:  bad packet rc=%d\n", rc);
-	    return 1;
-	}
-	puts("[main] INFO:  redeived packet");
-	dump(client_buffer, recv_len, true);
-	puts("\ncontent:");
-	dumpPacket(&inpkt);
     }
     return 0;
 }
