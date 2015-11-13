d03eabe15240e4c828590e974b3952d6ba21b740
JensErdmann <Jens.Erdmann@gmx.de>
2015-11-13 11:10:02 +0100

    Initial Light-Sensor TCS37727

--- LEDundSensoren/main.c
+++ LEDundSensoren/main.c
@@ -13,7 +13,7 @@
  * @file
  * @brief       test app phytec board
  *
- * @author      Kai
+ * @author      Kai and Jens
  *
  * @}
  */
@@ -28,11 +28,15 @@
 //#include "periph/spi.h"
 #include "periph/gpio.h"
 #include "ledcontrol.h"
+#include "tcs37727.h"
+
+#define SLEEP       (1000 * 1000U)
 
 
 char t2_stack[THREAD_STACKSIZE_MAIN];
 char t3_stack[THREAD_STACKSIZE_MAIN];
 char t4_stack[THREAD_STACKSIZE_MAIN];
+char t5_stack[THREAD_STACKSIZE_MAIN];
 
 unsigned int colorArray[30];
 
@@ -118,6 +122,41 @@ void *fourth_thread(void *arg)			//init hdc1000 und temp auslesen
 }
 
 
+void *fifth_thread(void *arg)			//sendet 20fps ans led band
+{
+    (void) arg;
+    
+	tcs37727_t dev;
+    	tcs37727_data_t data;
+
+	if (tcs37727_init(&dev, 0, TCS37727_I2C_ADDRESS,
+                      TCS37727_ATIME_DEFAULT) == 0) {
+        puts("[OK]\n");
+    	}
+    	else {
+        puts("[Failed]");
+        return NULL;
+	}
+    while(1)
+    {
+    if (tcs37727_set_rgbc_active(&dev)) {
+        		puts("Measurement start failed.");
+        		return NULL;
+    		}
+		tcs37727_read(&dev, &data);
+        	printf("R: %5"PRIu32" G: %5"PRIu32" B: %5"PRIu32" C: %5"PRIu32"\n",
+               	data.red, data.green, data.blue, data.clear);
+       	 	printf("CT : %5"PRIu32" Lux: %6"PRIu32" AGAIN: %2d ATIME %d\n",
+               	data.ct, data.lux, dev.again, dev.atime_us);
+
+        xtimer_usleep(SLEEP);
+	
+    }
+    
+    return NULL;
+}
+
+
 int main(void)
 {
 	int retVal = 0;
@@ -149,6 +188,11 @@ int main(void)
             t4_stack, sizeof(t4_stack),
             THREAD_PRIORITY_MAIN - 1, CREATE_WOUT_YIELD | CREATE_STACKTEST,
             fourth_thread, NULL, "nr4");
+
+	(void) thread_create(
+            t5_stack, sizeof(t5_stack),
+            THREAD_PRIORITY_MAIN - 1, CREATE_WOUT_YIELD | CREATE_STACKTEST,
+            fifth_thread, NULL, "nr5");
     	
     	while(1)
     	{
