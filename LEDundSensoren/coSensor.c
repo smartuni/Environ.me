aeec70c4d2ca79e2a3448c3761f298ba5ed15a6d
JensErdmann <Jens.Erdmann@gmx.de>
2016-01-08 11:15:48 +0100

    removed old folder

--- LEDundSensoren/coSensor.c
+++
@@ -1,7 +0,0 @@
-#include "coSensor.h"
-#include <math.h>
-
-float adcCoCalc(int value)
-{
-	return log((94.37751004f/value)-0.06084945844f)*1900/(-1.8971f)+100;
-}
--- LEDundSensoren/coSensor.h
+++
@@ -1,8 +0,0 @@
-#ifndef COSENSOR_H
-#define COSENSOR_H
-
-#include <math.h>
-
-float adcCoCalc(int value);
-
-#endif
