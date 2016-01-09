fb3efbbbc5174bd38c5e2e0446e9e95d2f9ee976
khhhh <kai.haehre@gmx.de>
2016-01-09 14:02:30 +0100

    Update ledcontrol.c

--- LEDandSensor/ledcontrol.c
+++ LEDandSensor/ledcontrol.c
@@ -5,22 +5,14 @@
 #include "periph/gpio.h"
 #include "board.h"
 
-void sendHigh(int dev)
-{
-	spi_transfer_byte(dev, 0b11110000, NULL);
-}
 
-void sendLow(int dev)
-{
-	spi_transfer_byte(dev, 0b00000000, NULL);
-}
 
-void writeLed(unsigned int array[], int led, unsigned char green, unsigned char red, unsigned char blue)
+void writeLed(unsigned int array[], int led, unsigned char green, unsigned char red, unsigned char blue)	//writes GRB values to a given array position
 {
 	array[led] = ((green<<16)|(red<<8)|blue);
 }
 
-void resetArray(unsigned int array[])
+void resetArray(unsigned int array[])	//writes all data in given array to zero
 {
 	for(int ind=0;ind<30;ind++)
 	{
@@ -28,27 +20,9 @@ void resetArray(unsigned int array[])
 	}
 }
 
-void sendArray(unsigned int array[], int dev)
-{
-	for(int ledPosition=0;ledPosition<30;ledPosition++)
-		{
-			for(int bitPosition=0;bitPosition<24;bitPosition++)
-			{
-				if(((array[ledPosition]>>(23-bitPosition))&1)==1)
-				{
-					sendHigh(dev);
-				}
-				else
-				{
-					sendLow(dev);
-				
-				}
-			}
-		}
-}
 
-
-void sendeNull(void)
+void sendeNull(void)	//sends bit pattern needed for detecting one logic Low (450ns High >0.7V, 800ns Low <0.3V) on led pin,
+			//timings change frequently with updates of Riot, so the ugly coding style means the only way to keep timings in needed conditions
 {
 	LED_R_OFF;
 	LED_R_OFF;
@@ -73,7 +47,8 @@ void sendeNull(void)
 	}
 }
 
-void sendeEins(void)
+void sendeEins(void)	//sends bit pattern needed for detecting one logic High (850ns High >0.7V, 400ns Low <0.3V) on led pin,
+			//timings change frequently with updates of Riot, so the ugly coding style means the only way to keep timings in needed conditions
 {
 	LED_R_OFF;
 	LED_R_OFF;
@@ -118,7 +93,7 @@ void sendeEins(void)
 	}
 }
 
-void sendeArray(unsigned int array[])
+void sendeArray(unsigned int array[])		//sends every bit in given data array on led pin
 {
 	for(int ledPosition=0;ledPosition<30;ledPosition++)
 		{
