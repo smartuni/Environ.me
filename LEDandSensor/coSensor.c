6bedc017bf594599e0fd066f7fb70a0e7cbff0b3
khhhh <kai.haehre@gmx.de>
2016-01-09 14:06:21 +0100

    Update coSensor.c

--- LEDandSensor/coSensor.c
+++ LEDandSensor/coSensor.c
@@ -1,7 +1,7 @@
 #include "coSensor.h"
 #include <math.h>
 
-float adcCoCalc(int value)
+float adcCoCalc(int value)	//calculates given int value from adc into real ppm value corresponding to co sensor MQ-7
 {
 	return log((94.37751004f/value)-0.06084945844f)*1900/(-1.8971f)+100;
 }
