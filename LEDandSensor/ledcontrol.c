713da872bc68defdacc4c5ad52218b6b940b5dd9
khhhh <kai.haehre@gmx.de>
2016-01-14 22:26:51 +0100

    Update ledcontrol.c

--- LEDandSensor/ledcontrol.c
+++ LEDandSensor/ledcontrol.c
@@ -21,25 +21,32 @@ void resetArray(unsigned int array[])	//writes all data in given array to zero
 }
 
 
-void sendZero(void)	//sends signal to led-controller which creates bit pattern needed for detecting one logic Low (350ns High [>0.7V / +-150ns], 800ns Low [<0.3V / +-150ns]) on led pin,
+void sendZero(void)	//sends bit pattern needed for detecting one logic Low (350ns High [>0.7V / +-150ns], 800ns Low [<0.3V / +-150ns]) on led pin,
 			//timings change frequently with updates of Riot, so the ugly coding style means the only way to keep timings in needed conditions
 {
-	for(int i=0;i<15;i++)
+	for(int i=0;i<3;i++)
 	{
-		LED_G_ON;
+		LED_R_OFF;
 	}
-	
-	LED_R_TOGGLE;
+
+	for(int i=0;i<10;i++)
+	{
+		LED_R_ON;
+	}
 }
 
-void sendOne(void)	//sends signal to led-controller which creates bit pattern needed for detecting one logic High (700ns High [>0.7V / +-150ns], 600ns Low [<0.3V / +-150ns]) on led pin,
+void sendOne(void)	//sends bit pattern needed for detecting one logic High (700ns High [>0.7V / +-150ns], 600ns Low [<0.3V / +-150ns]) on led pin,
 			//timings change frequently with updates of Riot, so the ugly coding style means the only way to keep timings in needed conditions
 {
-	for(int i=0;i<15;i++)
+	for(int i=0;i<4;i++)
 	{
-		LED_G_OFF;
+		LED_R_OFF;
 	}
-	LED_R_TOGGLE;
+	
+	for(int i=0;i<10;i++)
+	{
+		LED_R_ON;
+	}
 }
 
 void sendArray(unsigned int array[])		//sends every bit in given data array on led pin
